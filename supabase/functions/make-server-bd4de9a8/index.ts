3d671d235e6b40f4a75655b23e45c05052ce68f5
changohi195 <changohi195@gmail.com>
2025-11-19 19:19:47 +0900

    Revert "Export app from server module and serve via Supabase entry"
    This reverts commit 3ed73bb38cb61966d4488c110504339dde723660.

--- supabase/functions/make-server-bd4de9a8/index.ts
+++ supabase/functions/make-server-bd4de9a8/index.ts
@@ -1,3 +1,5 @@
-import app from '../../../src/supabase/functions/server/index.tsx';
-
-Deno.serve(app.fetch);
+/**
+ * Edge Function entry point for make-server-bd4de9a8.
+ * Re-exports the Hono app defined under src/supabase/functions/server.
+ */
+export { app as default } from '../../../src/supabase/functions/server/index.tsx';
