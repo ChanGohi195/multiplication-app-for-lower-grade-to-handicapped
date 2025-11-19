48da58a48af69ebca86e735bb236b3c507ee123c
changohi195 <changohi195@gmail.com>
2025-11-19 19:19:47 +0900

    Revert "Export Hono app via Supabase function entrypoint"
    This reverts commit 92f51188722f10596630d23bbd3c5be71dbfe2e9.

--- supabase/functions/make-server-bd4de9a8/index.ts
+++ supabase/functions/make-server-bd4de9a8/index.ts
@@ -1,5 +1,1 @@
-/**
- * Edge Function entry point for make-server-bd4de9a8.
- * Re-exports the Hono app defined under src/supabase/functions/server.
- */
-export { app as default } from '../../../src/supabase/functions/server/index.tsx';
+import '../../../src/supabase/functions/server/index.tsx';
